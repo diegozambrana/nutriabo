1f9ae6e47944c0d973bb258174986938325e1210
Diego Zambrana <contacto@diegozambrana.com>
2021-12-14 23:08:41 -0400

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Test} from './pages'
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
