2e3433b2109469cbb976ae2c86b6a9491374ab33
Diego Zambrana <contacto@diegozambrana.com>
2022-04-13 15:58:03 -0400

    added auth on models

---
+++ service/README.md
@@ -0,0 +1,11 @@
+# Servicios para NOTRIABO
+
+## Comandos
+
+### Cargar datos de tabla de alimentos de bolivia
+
+Descargar archivo `TABLA_DE_COMPOSICION_DE_ALIMENTOS_BOLIVIANA_2005.tsv` y correr el siguiente comando
+
+```
+python manage.py load_tsv_data --tsv_path=[DIRECTORY]/TABLA_DE_COMPOSICION_DE_ALIMENTOS_BOLIVIANA_2005.tsv
+```
---
+++ service/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    premium = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
