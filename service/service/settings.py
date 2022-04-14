4d3e6df40e2e884abcd109c783ab2b7083397b8a
Diego Zambrana <contacto@diegozambrana.com>
2022-04-14 08:49:36 -0400

    update requirements and add jwt libraries

--- service/service/settings.py
+++ service/service/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "graphene_django",
+
     'users',
     'food',
 ]
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
@@ -122,3 +125,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
+GRAPHENE = {
+    "SCHEMA": "service.schema.schema",
+    "MIDDLEWARE": [
+        "graphql_jwt.middleware.JSONWebTokenMiddleware",
+    ],
+}
+
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
