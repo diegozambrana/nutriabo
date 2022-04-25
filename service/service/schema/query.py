2b0a51fdbcb776fb2ee93496164c1bcc7170d2b3
Diego Zambrana <contacto@diegozambrana.com>
2022-04-25 07:38:25 -0400

    add query for categories and aliments

--- service/service/schema/query.py
+++ service/service/schema/query.py
@@ -1,10 +1,11 @@
 import graphene
 from service.schema.types import UserType, AlimentType, CategoryType
-from food.models import Alimento
+from food.models import Alimento, Categoria
 
 class Query(graphene.ObjectType):
     whoami = graphene.Field(UserType)
     all_aliments = graphene.List(AlimentType)
+    all_categories = graphene.List(CategoryType)
     get_aliment_by_code = graphene.Field(
         AlimentType,
         codigo=graphene.String(required=True)
@@ -18,6 +19,9 @@ class Query(graphene.ObjectType):
     
     def resolve_all_aliments(self, info):
         return Alimento.objects.all();
+
+    def resolve_all_categories(self, info):
+        return Categoria.objects.all()
 
     def resolve_get_aliment_by_code(self, info, codigo):
         try:
